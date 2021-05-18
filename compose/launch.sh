e1acb4c69845d2d5d90763e2be6e5dcca2ab531c
nicolas-hermosilla <nico31780@hotmail.fr>
2021-05-18 10:58:48 +0200

    remove test file

--- compose/launch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-WORKDIR='/var/lib/docker/compose/'
-
-docker-compose down --remove-orphans &&
-docker-compose --project-directory $WORKDIR \
-  up -d
--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-WORKDIR=$PWD
-CMD=docker-compose
-$CMD down --remove-orphans
-$CMD --project-directory $WORKDIR \
- 	up -d
